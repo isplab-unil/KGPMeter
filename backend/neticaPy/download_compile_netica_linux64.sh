a62e642ea96b550785308f3a0995aa8d1b8c446d
Didier Dupertuis <didier.dupertuis@unil.ch>
2019-11-13 15:32:10 +0100

    download_compile_netica_linux shell scripts correction

--- backend/neticaPy/download_compile_netica_linux64.sh
+++ backend/neticaPy/download_compile_netica_linux64.sh
@@ -4,11 +4,10 @@ curl -O https://www.norsys.com/downloads/NeticaAPI_Linux.zip
 unzip NeticaAPI_Linux.zip
 echo "compiling..."
 mkdir -p lib
-gcc -std=gnu11 -O2 -g -fPIC -pipe -I $1/lib/64bit/ $1/src/NeticaEx.c $1/lib/64bit/libnetica.a -shared -o lib/libnetica.so -lm -lpthread -lstdc++
+gcc -std=gnu11 -O2 -g -fPIC -pipe -I Netica_API_504/lib/64bit/ Netica_API_504/src/NeticaEx.c Netica_API_504/lib/64bit/libnetica.a -shared -o lib/libnetica.so -lm -lpthread -lstdc++
 
 echo "done compiling, removing files"
 rm NeticaAPI_Linux.zip
-rm -r Netica_API_504/
 
 
 
