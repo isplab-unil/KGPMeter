f9a6fab5f9677dfb5728b6970028273bcbbfcd21
Didier Dupertuis <didier.dupertuis@unil.ch>
2019-12-17 15:08:40 +0100

    set iframe style: width&height = 100%

--- frontend/lib/kgpmeter.js
+++ frontend/lib/kgpmeter.js
@@ -11,4 +11,6 @@ var KgpMeter = function KgpMeter(divId, apiUrl, lang) {
 
   this.div = document.getElementById(divId);
   this.div.innerHTML = "<iframe src='{src}/app/'></iframe>".replace("{src}", this.apiUrl);
+  this.iframe = this.div.getElementsByTagName("iframe")[0];
+  this.iframe.setAttribute("style", 'border:none; width:100%; height:100%;');
 };
--- frontend/lib/src/kgpmeter.js
+++ frontend/lib/src/kgpmeter.js
@@ -9,5 +9,7 @@ class KgpMeter{
 
     this.div = document.getElementById(divId)
     this.div.innerHTML = "<iframe src='{src}/app/'></iframe>".replace("{src}",this.apiUrl)
+    this.iframe = this.div.getElementsByTagName("iframe")[0]
+    this.iframe.setAttribute("style",'border:none; width:100%; height:100%;')
   }
 }
