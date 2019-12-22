bdc1be3b8d37b000ceb8f620f23cc1b51ea74c5e
Didier Dupertuis <didier.dupertuis@unil.ch>
2019-12-22 10:11:10 +0100

    KgpIframInterface.js skeleton

---
+++ frontend/app/src/js/KgpIframeInterface.js
@@ -0,0 +1,29 @@
+
+
+
+export class KgpInnerClient{
+
+}
+
+
+export class KgpOuterClient{
+  
+}
+
+export class KgpSetSourceEvent extends CustomEvent{
+
+}
+
+export class KgpSetLanguageEvent extends CustomEvent{
+
+}
+
+/** Event from kgp-iframe to kgpmeter to change height */
+export class KgpSetHeightEvent extends CustomEvent{
+
+}
+
+/** Event from kgpmeter to kgp-iframe to signale iframe max dimensions */
+export class KgpSetIframeDimensionEvent extends CustomEvent{
+
+}
