86e97756c0cd4e168aac53d79fee8f41a1facda6
Didier Dupertuis <didier.dupertuis@unil.ch>
2020-02-03 15:03:50 +0100

    lang -> lng

--- frontend/lib/src/js/kgpmeter.js
+++ frontend/lib/src/js/kgpmeter.js
@@ -2,10 +2,10 @@ import {kgpSetLanguageEvent, kgpSetSourceEvent, kgpSetIframeMaxDimensionEvent} f
 
 
 class KgpMeter{
-  constructor(divId, apiUrl, lang, maxHeight){
+  constructor(divId, apiUrl, lng, maxHeight){
     this.divId = divId
     this.apiUrl = apiUrl
-    this.lang = lang
+    this.lng = lng
     let self = this
 
     this.div = document.getElementById(divId)
@@ -13,15 +13,13 @@ class KgpMeter{
     this.iframe = this.div.getElementsByTagName("iframe")[0]
     this.iframe.setAttribute("style",'border:none; width:100%; height:100%;')
 
-    console.log("this.lang", self.lang)
 
     // ======== send data to iframe ========
     this.iframe.contentWindow.addEventListener("load",()=>{
       setTimeout(() => {
         console.log("self.iframe.contentWindow LOADED!!")
         // set language
-        let setLanguageEvent = kgpSetLanguageEvent(self.lang)
-        self.iframe.contentDocument.dispatchEvent(setLanguageEvent)
+        this.changeLanguage(self.lng)
         // set source
         let setSourceEvent = kgpSetSourceEvent(document.URL)
         self.iframe.contentDocument.dispatchEvent(setSourceEvent)
@@ -45,6 +43,11 @@ class KgpMeter{
       console.log("Communication kgp-iframe to kgpmeter success! detail:", e.detail) // outputs: {foo: 'bar'}
     }
     window.document.addEventListener('myCustomEvent', handleEvent, false)
+  }
+
+  changeLanguage(lng){
+    let setLanguageEvent = kgpSetLanguageEvent(self.lng)
+    self.iframe.contentDocument.dispatchEvent(setLanguageEvent)
   }
 }
 // export KgpMeter to glboal namespace
