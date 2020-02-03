3fa6df12da38f913c98d685195885d3291f163a4
Didier Dupertuis <didier.dupertuis@unil.ch>
2020-02-03 13:58:40 +0100

    added i18n prefix in kgp app.js

--- frontend/app/src/js/app.js
+++ frontend/app/src/js/app.js
@@ -38,7 +38,7 @@ function onChangeLanguage(oldLng, newLng){
   setTimeout(()=>{ d3.selectAll(".ext-link").attr("target","blank") },1)
 }
 
-let i18n = new Internationalisation(["en","fr","de","it","es"], languageLoader, cookie.read("lng"), true)
+let i18n = new Internationalisation(["en","fr","de","it","es"], languageLoader, cookie.read("lng"), true,"kgpmeter.")
 i18n.languageChangeCallbacks.push(onChangeLanguage)
 i18n.observe(document)
 //i18n.dynamic["cookie-text"] = (t,d) => t.replace("{#1}",Boolean(document.URL.match(/\/privacy-dev\//))? "/privacy-dev" : "/privacy")
