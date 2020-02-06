f409b8bd088b8bb709e332a0804c51d4ca586f34
Didier Dupertuis <didier.dupertuis@unil.ch>
2020-02-06 15:47:27 +0100

    removed kgp iframe interface unused code

--- frontend/app/src/js/KgpIframeInterface.js
+++ frontend/app/src/js/KgpIframeInterface.js
@@ -1,57 +1,6 @@
 import {cookie} from "./lib/cookies.js"
 let cl = console.log
 
-export class KgpInnerClient{
-  constructor(i18n, sourceCookieName, kgp){
-
-    // set language event
-    function setLanguage(e){
-      cl("-- KgpInnerClient setLanguage()!")
-      i18n.changeLanguage(e.lng)
-    }
-    window.document.addEventListener('KgpSetLanguageEvent', setLanguage, false)
-
-    // set source event
-    function setSource(e){
-      cl("-- KgpInnerClient setsource()!")
-      let userSource = cookie.read(sourceCookieName)
-      if(!userSource){
-        cookie.create(sourceCookieName, e.source, 1)
-      }
-    }
-    window.document.addEventListener('KgpSetSourceEvent', setSource, false)
-
-    // set max dimensions event
-    function setIframeMaxDimensionEvent(e){
-      cl("-- KgpInnerClient setIframeMaxDimensionEvent()!")
-    }
-    window.document.addEventListener('KgpSetIframeMaxDimensionEvent', setIframeMaxDimensionEvent, false)
-  }
-}
-
-
-export class KgpOuterClient{
-  constructor(iframeElementid, language, max_height){
-    this.iframe = document.getElementById(iframeElementid)
-    this.userSource = document.URL
-
-    let self = this
-    self.iframe.contentDocument.onload = ()=>{
-      console.log("self.iframe.contentDocument LOADED!!")
-      // set language
-      let setLanguageEvent = kgpSetLanguageEvent(language)
-      self.iframe.contentDocument.dispatchEvent(setLanguageEvent)
-      // set source
-      let setSourceEvent = kgpSetSourceEvent(document.URL)
-      self.iframe.contentDocument.dispatchEvent(setSourceEvent)
-      // set max height
-      let setIframeMaxDimensionEvent = kgpSetIframeMaxDimensionEvent(max_height)
-      self.iframe.contentDocument.dispatchEvent(setIframeMaxDimensionEvent)
-    }
-
-  }
-  
-}
 
 /******** down events ********/
 
