7785acbf647b655418abe36413858cc1691dd456
Didier Dupertuis <didier.dupertuis@unil.ch>
2020-02-13 14:22:07 +0100

    failsafe cookies read/write with default value in case of failure

--- frontend/app/src/js/lib/cookies.js
+++ frontend/app/src/js/lib/cookies.js
@@ -1,31 +1,51 @@
-export let cookie = {
-  create: function createCookie(name, value, days) {
-    var expires;
+function createCookie(name, value, days) {
+  var expires;
 
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    } else {
-        expires = "";
-    }
-    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
-  },
-
-  read: function readCookie(name) {
-    var nameEQ = encodeURIComponent(name) + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ')
-            c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0)
-            return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-    return null;
-  },
-
-  erase: function eraseCookie(name) {
-    document.cookie = encodeURIComponent(name) + "=;expires=" + (new Date()).toGMTString() + "; path=/";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+  } else {
+      expires = "";
   }
+  document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = encodeURIComponent(name) + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ')
+          c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0)
+          return decodeURIComponent(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = encodeURIComponent(name) + "=;expires=" + (new Date()).toGMTString() + "; path=/";
+}
+
+function tryOrNull(func, defaultVal=null){
+  console.log("cookie tryOrNull() with func: ", func)
+  return function(a,b,c){
+    try{
+      return func(a,b,c)
+    }catch(e){
+      console.error("Unable to access cookies with function ", func)
+      return defaultVal
+    }
+  }
+}
+
+export let cookie = {
+  create: createCookie,
+  read: readCookie,
+  erase: eraseCookie,
+  // versions that try to create/read/erase and return null upon failur
+  tryCreate: tryOrNull(createCookie),
+  tryRead: tryOrNull(readCookie),
+  tryErase: tryOrNull(eraseCookie),
 }
